692ecc4ae40d585f318f7c47c42c2ebe052da5c9
SlaughterBruce-FS <116940375+SlaughterBruce-FS@users.noreply.github.com>
2025-04-07 14:53:59 -0400

    Initial Commit

---
+++ Recipe_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe_App
+//
+//  Created by jade  on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe_AppTests/Recipe_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppTests.swift
+//  Recipe_AppTests
+//
+//  Created by jade  on 4/7/25.
+//
+
+import Testing
+@testable import Recipe_App
+
+struct Recipe_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
